f5bfc51990e4602fe3439e45f612c92dc263cd08
LuisFerSH <lfsanchezhernandez@gmail.com>
2026-08-21 16:39:09 -0600

    inicia el repo. Se sube todo actualizado.

---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,9 @@
+let numero = 64;
+
+function calcularRaizCuadrada(numero){
+    let resultado = Math.sqrt(numero);
+    console.log(resultado); // Necesario para imprimir en consola con Node
+    return resultado;
+}
+
+calcularRaizCuadrada(numero);
